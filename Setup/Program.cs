2a817e961c786636eae183e39dedd1133f3f9db7
JT Clarke <jacksonclarke2001@hotmail.com>
2022-09-19 21:04:02 -0600

    setup tutorial 1

--- Setup/Program.cs
+++ Setup/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("What is your favorite color?");
+
+        string color = Console.ReadLine();
+
+        Console.WriteLine($"Your favorite color is {color}.");
+    }
+}
