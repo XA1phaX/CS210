3279889e57e46a2ef8188d376149723cb9dcedc5
JT Clarke <jacksonclarke2001@hotmail.com>
2022-10-04 11:00:45 -0600

    push oct04

---
+++ Test/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    
+    class Program
+    {     
+    static Dictionary<int, List<int>> WinConditions = new Dictionary<int, List<int>>()
+    {
+        { 1, new List<int>() {1, 2, 3 } },
+        { 2, new List<int>() {4, 5, 6 } },
+        { 3, new List<int>() {7, 8, 9 } },
+        { 4, new List<int>() {1, 4, 7 } },
+        { 5, new List<int>() {2, 5, 8 } },
+        { 6, new List<int>() {3, 6, 9 } },
+        { 7, new List<int>() {1, 5, 9 } },
+        { 8, new List<int>() {3, 5, 7 } }
+    };
+
+
+    static void Main(string[] args)
+    {
+        checkDictionary();
+    }
+
+    public static void checkDictionary()
+    {
+        foreach (KeyValuePair<int, List<int>> entry in WinConditions)
+        {
+            // Console.WriteLine("Key = {0}, Value = {1}", entry.Key, entry.Value);
+            Console.WriteLine(entry.Value[1]);
+        }
+
+        // for (var i = 1; i < WinConditions.Count; i++) 
+        // {
+        //     Console.WriteLine("Key = is {0}, Value = {1}", WinConditions[i], WinConditions.Values);
+        // }
+    }
+    }
+
+}
+    
+
+
+
---
+++ dice/Unit02/Game/Die.cs
@@ -0,0 +1,57 @@
+using System;
+
+
+namespace Unit02.Game
+{
+    // TODO: Implement the Die class as follows...
+    // 1) Add the class declaration. Use the following class comment.
+    // 2) Add the following private fields:
+
+        /// <summary>
+        /// A small cube with a different number of spots on each of its six sides.
+        /// The responsibility of Die is to keep track of its currently rolled value and the points its
+        /// worth.
+        /// </summary> 
+    public class Die{
+        public int _value;
+        public int _points;
+
+        /// <summary>
+        /// Gets the value of the die.
+        /// </summary>
+
+
+     // 2) Create the class constructor. Use the following method comment.
+        public Die(){
+        /// <summary>
+        /// Constructs a new instance of Die.
+        /// </summary>
+        }
+
+     // 3) Create the Roll() method. Use the following method comment.        
+        public void Roll(){
+            Random random = new Random();
+            _value = random.Next(1,7);
+            if(_value ==1){
+                _points = 100;
+            }
+            else if(_value == 5){
+                _points = 50;
+            }
+            else{
+                _points = 0;
+            }
+
+        /// <summary>
+        /// Generates a new random value and calculates the points for the die. Fives are 
+        /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
+        /// </summary>
+        }
+    }
+
+
+}
+
+    
+   
+        
